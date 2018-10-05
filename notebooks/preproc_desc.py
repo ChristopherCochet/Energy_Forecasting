86db034bb0a0359635325a8b79170ceaecd2f016
jsprovost1 <provostjs@gmail.com>
2018-10-05 17:47:08 -0600

    Update preproc_desc.py

--- notebooks/preproc_desc.py
+++ notebooks/preproc_desc.py
@@ -7,37 +7,41 @@ path = '/Users/jean-sebastienprovost/Desktop/coursera/kaggle/energy_forecasting'
 os.chdir(path)
 
 
-
 class Data:
 
     def __init__(self):
         self.ind_col = 0
 
-    def preview_data(self, data, nrow=None):
-        df = self._load_data(data).head(nrow)
-        print(df)
+        # NEED TO FIX => df1 and df2 are coming from a previous pd.read_csv and not from a method from this class
 
-    def extract_spec_val(self, data, col_id, series_id, extract_col):
-        df = self._load_data(data)
-        return df[df[col_id] == series_id][extract_col]
+    def getMerge(self, df1, df2, df2_features, ontarget):
+        df1 = pd.merge(df1, df2[df2_features], on=ontarget)
+        return df1
 
-    def desc_stats(self, data, col=None):
-        return self._load_data(data)[col].describe()
+    def getBoxplot(self, x, y, hue=None):
+        p = sns.boxplot(x=x, y=y, data=self.df, hue=None)
+        return p
 
-    def min_data(self, data, col):
-        return self._load_data(data)[col].min()
+    def preview_data(self, nrow=None):
+        return self.df.head(nrow)
 
-    def max_data(self, data, col):
-        return self._load_data(data)[col].max()
+    def extract_spec_val(self, col_id, series_id, extract_col):
+        return self.df[self.df[col_id] == series_id][extract_col]
 
-    def show_data(self, data):
-        df = self._load_data(data)
-        return df
+    def desc_stats(self, col=None):
+        return self.df[col].describe()
+
+    def get_min(self, col):
+        return self.df[col].min()
+
+    def get_max(self, col):
+        return self.df[col].max()
+
+    def show_data(self):
+        print(self.df)
 
     def _load_data(self, file):
-        return pd.read_csv(file, index_col=self.ind_col)
+        self.df = pd.read_csv(file, index_col=0)
 
 
 data = Data()
-
-data.preview_data("consumption_train.csv",10)
